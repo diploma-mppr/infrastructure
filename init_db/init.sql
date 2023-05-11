07b9ddab01a86a015925a18ce7ecb472408559a6
yut_fut <seregey.golubev@mail.ru>
2023-05-11 15:30:20 +0300

    added nginx image

--- init_db/init.sql
+++ init_db/init.sql
@@ -1,10 +1,18 @@
 DROP TABLE IF EXISTS pareto CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 SET timezone TO '+03';
 
-create table tdata
-(
+create table method (
     id      integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id integer NOT NULL,
+    method_name  varchar(256) NOT NULL,
     name    VARCHAR(256) NOT NULL,
     data    bytea
-)
+);
+
+CREATE TABLE users (
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL
+);
